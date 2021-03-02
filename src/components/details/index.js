46906ee97bf7a27403007db91e872139fa07ab7e
bmalkmus <bmalkmus23@gmail.com>
2021-03-01 16:33:33 -0800

    regex added for spaces in URL

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -12,10 +12,9 @@ const Details = ({match}) => {
     const params = match.params.title
     const divider = params.indexOf(":")
     const id = parseInt(params.slice(0, divider))
-    const title = params.slice(divider+1)
+    const title = params.slice(divider+1).replace(/%20/g, " ")
     const history = useHistory();
 
-    console.log(sauces)
     
     const sauce = sauces.filter((sauce)=>sauce.title === title && sauce.id===id)
     if(sauce.length === 0) {
